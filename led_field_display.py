e840de36a2cab579a1bcdda16dad25cacbd82dea
Mark Zakharyan <markzakharyan@ucsb.edu>
2025-05-13 17:08:49 -0700

    added led field display

---
+++ led_field_display.py
@@ -0,0 +1,37 @@
+import labrad
+import serial
+import time
+import sys, signal
+
+cxn = labrad.connect()
+mag = cxn.ami_430
+mag.select_device()
+
+signal.signal(signal.SIGINT, signal.default_int_handler)
+led = serial.Serial('COM8',9600,timeout=0.2)
+led.flush()
+time.sleep(2)
+
+isramping = mag.state()
+if isramping == 1:
+    led.write(b"m1")
+else:
+    led.write(b"m0")
+
+led.write(b'c0,2,1')
+
+while True:
+    try:
+        led.write(("f" + mag.get_field_mag()[0:5]).encode())
+
+        if isramping != mag.state():
+            isramping = mag.state()
+            if isramping == 1:
+                led.write(b"m1")
+            else:
+                led.write(b"m0")
+        time.sleep(2)
+    except KeyboardInterrupt:
+        led.write("f(-_-)")
+        led.close()
+        sys.exit()
